307414a93528eef5e44785b72d933fbe6399510c
Serge Smetana <serge.smetana@gmail.com>
2018-03-15 13:16:08 +0200

    Simplify example

--- _examples/example.go
+++ _examples/example.go
@@ -6,48 +6,34 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func mainLoop(ebox *editbox.Editbox) {
-	eventQueue := make(chan termbox.Event, 256)
-	go func() {
-		for {
-			eventQueue <- termbox.PollEvent()
-		}
-	}()
-	for {
-		select {
-		case ev := <-eventQueue:
-			ok := ebox.HandleEvent(&ev)
-			if !ok {
-				return
-			}
-			if len(eventQueue) == 0 {
-				ebox.Draw()
-				termbox.Flush()
-			}
-		}
-	}
-}
-
 func main() {
 	err := termbox.Init()
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 	defer termbox.Close()
-	termbox.SetInputMode(termbox.InputEsc)
-	termbox.SetOutputMode(termbox.Output256)
-	fmt.Println("Editor Test\n\n\n\nEnter text here:")
-	ebox := editbox.NewEditbox(17, 4, 25, 3, editbox.Options{
+	fmt.Println("\n  Type here:")
+
+	ebox := editbox.NewEditbox(13, 1, 25, 3, editbox.Options{
 		Wrap:       true,
 		Autoexpand: true,
 		MaxHeight:  6,
-		Fg:         12,
-		Bg:         9})
-	ebox.Draw()
+		Fg:         termbox.ColorWhite,
+		Bg:         termbox.ColorBlue})
+
+	// ebox.Render() only puts its editor's content into termbox
+	// CellBuffer but does not flush it
 	termbox.Flush()
-	mainLoop(ebox)
+
+	for {
+		ev := termbox.PollEvent();
+		if ev.Type == termbox.EventKey {
+			ok := ebox.HandleEvent(&ev)
+			if !ok {
+				return // Quit
+			}
+		}
+		ebox.Render()
+		termbox.Flush()
+	}
 }
